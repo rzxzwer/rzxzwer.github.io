eb36cc751f0ecabc832e7de56abd6b086100b3d9
Тимур <165780980+rzxzwer@users.noreply.github.com>
2024-06-12 15:17:05 +0700

    Add files via upload

---
+++ генератор паролей.py
@@ -0,0 +1,35 @@
+# генератор пароля
+
+import random
+
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+           'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
+           'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Добро подаловать в генератор паролей!")
+nr_letters = int(input("сколько букв должно быть в вашем пароле?\n"))
+nr_symbols = int(input(f"сколько символов ?\n"))
+nr_numbers = int(input(f" и сколько чисел?\n"))
+
+password_list = []
+
+for char in range(1, nr_letters + 1):
+    password_list.append(random.choice(letters))
+
+for char in range(1, nr_symbols + 1):
+    password_list += random.choice(symbols)
+
+for char in range(1, nr_numbers + 1):
+    password_list += random.choice(numbers)
+
+print(password_list)
+random.shuffle(password_list)
+print(password_list)
+
+password = ""
+for char in password_list:
+    password += char
+
+print(f"Your password is: {password}")
